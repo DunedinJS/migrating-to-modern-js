25a16be2b290b28a026846ea7355ac40d417f2b2
Matthew Norrish <matt.norrish@clavisinsight.com>
2016-12-12 12:29:52 +1300

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+
+  entry: './app.js',
+
+  output: {
+    path: './dist', // the directory to output bundled files to
+    publicPath: 'dist', // the web path prefix for bundled files
+    filename: 'bundle.js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015'],
+        },
+      },
+    ],
+  },
+};
